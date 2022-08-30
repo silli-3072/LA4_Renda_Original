bf75f7397af650cde02fd2311425515dae9d1966
MinoriHaruta <hrtmnr.3072@gmail.com>
2022-08-30 16:02:40 +0900

    Initial Commit

---
+++ Renda_Original/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Renda_Original
+//
+//  Created by 春田実利 on 2022/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
